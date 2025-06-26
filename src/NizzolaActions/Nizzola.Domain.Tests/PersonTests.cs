93c3c0047daf4de850d608dc3607e369bac1f661
Marcio Rogerio Nizzola <marcio@nizzola.com.br>
2025-06-25 21:10:07 -0300

    first commit

---
+++ src/NizzolaActions/Nizzola.Domain.Tests/PersonTests.cs
@@ -0,0 +1,102 @@
+﻿using FluentValidation.TestHelper;
+using Nizzola.Domain.Models;
+using Nizzola.Domain.Validators;
+
+namespace Nizzola.Domain.Tests;
+
+public class PersonTests
+{
+    private readonly PersonValidator _validator;
+
+    public PersonTests()
+    {
+        _validator = new PersonValidator();
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_FirstName_Is_Empty()
+    {
+        var person = new Person { FirstName = string.Empty };
+        var result = _validator.TestValidate(person);
+        result.ShouldHaveValidationErrorFor(p => p.FirstName);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_FirstName_Exceeds_MaxLength()
+    {
+        var person = new Person { FirstName = new string('A', 51) };
+        var result = _validator.TestValidate(person);
+        result.ShouldHaveValidationErrorFor(p => p.FirstName);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_LastName_Is_Empty()
+    {
+        var person = new Person { LastName = string.Empty };
+        var result = _validator.TestValidate(person);
+        result.ShouldHaveValidationErrorFor(p => p.LastName);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_LastName_Exceeds_MaxLength()
+    {
+        var person = new Person { LastName = new string('A', 51) };
+        var result = _validator.TestValidate(person);
+        result.ShouldHaveValidationErrorFor(p => p.LastName);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_DateOfBirth_Is_In_The_Future()
+    {
+        var person = new Person { DateOfBirth = DateTime.Now.AddDays(1) };
+        var result = _validator.TestValidate(person);
+        result.ShouldHaveValidationErrorFor(p => p.DateOfBirth);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_Email_Is_Empty()
+    {
+        var person = new Person { Email = string.Empty };
+        var result = _validator.TestValidate(person);
+        result.ShouldHaveValidationErrorFor(p => p.Email);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_Email_Is_Invalid()
+    {
+        var person = new Person { Email = "invalid-email" };
+        var result = _validator.TestValidate(person);
+        result.ShouldHaveValidationErrorFor(p => p.Email);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_PhoneNumber_Is_Empty()
+    {
+        var person = new Person { PhoneNumber = string.Empty };
+        var result = _validator.TestValidate(person);
+        result.ShouldHaveValidationErrorFor(p => p.PhoneNumber);
+    }
+
+    //[Fact]
+    //public void Should_Have_Error_When_PhoneNumber_Is_Invalid()
+    //{
+    //    var person = new Person { PhoneNumber = "12345" };
+    //    var result = _validator.TestValidate(person);
+    //    result.ShouldHaveValidationErrorFor(p => p.PhoneNumber);
+    //}
+
+    [Fact]
+    public void Should_Not_Have_Error_When_All_Fields_Are_Valid()
+    {
+        var person = new Person
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            DateOfBirth = DateTime.Now.AddYears(-30),
+            Email = "john.doe@example.com",
+            PhoneNumber = "+1234567890"
+        };
+        var result = _validator.TestValidate(person);
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
---
+++ src/NizzolaActions/Nizzola.Domain/Validators/PersonValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Nizzola.Domain.Models;
+
+namespace Nizzola.Domain.Validators;
+public class PersonValidator : AbstractValidator<Person>
+{
+    public PersonValidator()
+    {
+        RuleFor(person => person.FirstName)
+            .NotEmpty().WithMessage("First name is required.")
+            .MaximumLength(50).WithMessage("First name must not exceed 50 characters.");
+
+        RuleFor(person => person.LastName)
+            .NotEmpty().WithMessage("Last name is required.")
+            .MaximumLength(50).WithMessage("Last name must not exceed 50 characters.");
+
+        RuleFor(person => person.DateOfBirth)
+            .LessThan(DateTime.Now).WithMessage("Date of birth must be in the past.");
+
+        RuleFor(person => person.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.");
+
+        RuleFor(person => person.PhoneNumber)
+            .NotEmpty().WithMessage("Phone number is required.")
+            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Invalid phone number format.");
+    }
+}
